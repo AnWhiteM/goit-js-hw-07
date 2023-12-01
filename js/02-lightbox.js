f45d8a6d3f65113959da78a5cb35a02403b2428a
Ivan Grivtsov <ivangrivtsov0@gmail.com>
2023-12-01 21:44:37 +0200

    2nd-task-cmpltd

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const galleryContainer = document.getElementById("gallery");
+
+  function renderGallery(items) {
+    items.forEach((item) => {
+      const galleryItem = document.createElement("li");
+      galleryItem.classList.add("gallery__item");
+
+      const link = document.createElement("a");
+      link.classList.add("gallery__link");
+      link.href = item.original;
+
+      const image = document.createElement("img");
+      image.classList.add("gallery__image");
+      image.src = item.preview;
+      image.alt = item.description;
+
+      link.appendChild(image);
+      galleryItem.appendChild(link);
+      galleryContainer.appendChild(galleryItem);
+    });
+  }
+
+  renderGallery(galleryItems);
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+
+  lightbox.on("show.simplelightbox", function (e) {
+    console.log("Lightbox is shown");
+  });
+
+  lightbox.on("close.simplelightbox", function (e) {
+    console.log("Lightbox is closed");
+  });
+});
 
 console.log(galleryItems);
